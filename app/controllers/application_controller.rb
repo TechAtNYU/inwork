53f234f74ef1f6ca30306740a9dcb525755c18de
Nate Berkopec <nate.berkopec@gmail.com>
2013-02-07 21:06:16 -0500

    Password/username auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  USERS = { ENV['APP_USERNAME'] => ENV['APP_PASSWORD'] }
+ 
+  before_filter :authenticate
+ 
+  private
+ 
+  def authenticate
+    return true unless Rails.env.production? #production only
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
