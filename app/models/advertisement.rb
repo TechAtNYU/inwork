e7b08a257825c4ccd8e29c3c220bb843702b1651
Nate Berkopec <nate.berkopec@gmail.com>
2013-02-08 16:13:45 -0500

    BootStrapp'd

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -2,4 +2,8 @@ class Advertisement < ActiveRecord::Base
   attr_accessible :ad_target_url, :alt_text, :title, :image
   attr_accessible :image
   has_attached_file :image, :styles => { :regular => "500x140>" }
+
+  def embed_string
+    "<a href='#{ad_target_url}'><img alt='#{alt_text}' src='#{image.url(:regular)}'></a>"
+  end
 end
