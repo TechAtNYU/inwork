505d6954568dc021c4d0c1ab648acd5de9e1b1c7
Nate Berkopec <nate.berkopec@gmail.com>
2013-02-08 16:44:02 -0500

    Move secret_token into ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Inwork::Application.config.secret_token = '3be54a5f56dd9b38b41c5c92f87ef93b582069554ece62a8ce0ce5ca5756ebb62e027611153bd516b190ec6cbcde24ff038c70eaa5a56a3652c5587b1c9c3211'
+Inwork::Application.config.secret_token = ENV['SECRET_TOKEN']
